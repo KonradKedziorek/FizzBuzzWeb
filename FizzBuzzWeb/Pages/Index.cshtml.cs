bac2253fa2739bd0a98cd9e5a7bdf2d946e203fa
Konrad Kędziorek <konradk453@gmail.com>
2021-04-12 22:14:04 +0200

    Adding datas to database finally works

--- FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -8,20 +8,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using FizzBuzzWeb.Pages.Models;
+using FizzBuzzWeb.Data;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
+        private FizzBuzzContext fizzBuzzContext;
         private readonly ILogger<IndexModel> _logger;
 
         [BindProperty]
         public FizzBuzz FizzBuzz { get; set; }
 
-        public string result = "";
+        //public string result = "";
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, FizzBuzzContext context)
         {
+            fizzBuzzContext = context;
             _logger = logger;
         }
 
@@ -34,6 +37,10 @@ namespace FizzBuzzWeb.Pages
             if (ModelState.IsValid)
             {
                 HttpContext.Session.SetString("SessionFizzBuzz", JsonConvert.SerializeObject(FizzBuzz));
+                FizzBuzz.result = FizzBuzz.FizzBuzzApp();
+                fizzBuzzContext.Add(FizzBuzz);
+                fizzBuzzContext.SaveChanges();
+                FizzBuzz.result = "";
                 return Page();
             }
             return Page();
